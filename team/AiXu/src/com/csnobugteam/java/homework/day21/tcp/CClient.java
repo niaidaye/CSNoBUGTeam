e36d4ab79d30fe365b9f6ebb05ef84eee7b28ba4
AiXu <645992178@qq.com>
2023-11-08 09:17:55 +0800

    完成作业题目2 参考课上代码 a. 客户端键盘录入，服务器输出文本文件 b. 客户端文本文件，服务器输出到控制台 c. 客户端文本文件，服务器输出文本文件 在文件tcp包下， Axxx.class,Bxxx.class,Cxxx.class 分别代表a,b,c

---
+++ team/AiXu/src/com/csnobugteam/java/homework/day21/tcp/CClient.java
@@ -0,0 +1,40 @@
+package com.csnobugteam.java.homework.day21.tcp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * c. 客户端文本文件，服务器输出文本文件
+ * 在客户端，读取要发送的文本文件内容，并将其发送到服务器。
+ * @author aixu
+ * @since 2023/11/8
+ */
+public class CClient {
+    public static void main(String[] args) {
+        try {
+            // 创建 Socket 对象，指定服务器的 IP 和端口号
+            Socket socket = new Socket("127.0.0.1", 8888);
+
+            // 获取输出流，用于向服务器发送数据
+            OutputStream outputStream = socket.getOutputStream();
+
+            // 读取文本文件内容
+            Path path = Paths.get("input.txt");
+            byte[] data = Files.readAllBytes(path);
+
+            // 发送数据给服务器
+            outputStream.write(data);
+
+            // 关闭资源
+            outputStream.close();
+            socket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
