c7313eb06a8b2027e05039c0aeb1656dd0596353
aixu <645992178@qq.com>
2023-11-17 23:09:04 +0800

    修改ListNode报错信息，增加Node节点

---
+++ team/AiXu/src/com/csnobugteam/java/utils/ListNode.java
@@ -0,0 +1,13 @@
+package com.csnobugteam.java.utils;
+
+/**
+ * List链表节点
+ *
+ * @author aixu
+ * @since 2023/11/17
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
