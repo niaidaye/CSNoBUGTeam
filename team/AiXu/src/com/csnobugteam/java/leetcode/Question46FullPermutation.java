eb729ff22ba173b19c4f700a58ff99d43673012d
A_X <645992178@qq.com>
2023-11-24 09:09:42 +0800

    添加leetcode上的一些题目如：题目01、46、509

---
+++ team/AiXu/src/com/csnobugteam/java/leetcode/Question46FullPermutation.java
@@ -0,0 +1,43 @@
+package editor.cn;
+
+import java.util.LinkedList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    List<List<Integer>> res = new LinkedList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        // 记录路径
+        LinkedList<Integer> track = new LinkedList<>();
+        // 标记是否重复使用
+        boolean[] used = new boolean[nums.length];
+
+        backTrack(nums, track, used);
+        return res;
+    }
+
+    private void backTrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+        // 递归结束条件
+        if (track.size() == nums.length) {
+            // 把记录路径添加到res中
+            res.add(new LinkedList<>(track));
+            return;
+        }
+
+        // 进行递归前校验
+        for (int i = 0; i < nums.length; i++) {
+            // 排除nums中已经使用过的元素
+            if (used[i]) continue;
+            // 进行路径选择
+            track.add(nums[i]);
+            // 标记已经被选择
+            used[i] = true;
+            // 递归进入下层决策树
+            backTrack(nums, track, used);
+            // 退出决策树，进行撤销选择
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ team/AiXu/src/com/csnobugteam/java/leetcode/Question509TheFibonacciNumsDP.java
@@ -0,0 +1,23 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int fib(int n) {
+        // 暴力算法
+//        if( n == 1 || n == 2) return 1;
+//        return fib(n -1) + fib(n - 2);
+        // 带备忘录的递归
+        int memo[] = new int[n + 1];
+        return helpr(memo, n);
+    }
+
+    public int helpr(int[] memo, int n) {
+        // base case
+        if (n == 0 || n == 1) return n;
+        // 已经计算过，不用计算
+        if(memo[n] != 0) return memo[n];
+        // 递归体
+        return helpr(memo, n - 1) + helpr(memo, n - 2);
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
