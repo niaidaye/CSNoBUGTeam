3a8eebc6bdf454595aa1678747f8121211983302
stephen <sctuerprhyen@qq.com>
2023-12-08 20:21:54 +0800

    更新了mybatis作业

---
+++ team/ZhouMuYang/src/Homework/mybatis/bean/Account.java
@@ -0,0 +1,10 @@
+package Homework.mybatis.bean;
+public class Account {
+    private Integer id;
+    private String name;
+    private String address;
+    private String email;
+    private String phone;
+    private String age;
+
+}
---
+++ team/ZhouMuYang/src/Homework/mybatis/mapper/AccountMapper.java
@@ -0,0 +1,15 @@
+package Homework.mybatis.mapper;
+import java.util.List;
+
+public interface AccountMapper {
+//    List<Account> queryAccountByNameAndAddress(@Param("name") String name, @Param("address") String address);
+//
+//    // 如果传了 名字就根据名字查； 如果传了address，就拼接address；如果传了email就根据email查
+//    List<Account> queryAccountByCondition(Account account);
+//
+//    // 如果传了id，就根据id查； 否则，如果名字不为空，根据name来查；  否则，根据手机号查
+//    List<Account> queryAccountByCondition2(Account account);
+//
+//    // 如果年龄输入小于0.则不拼接；如果在18岁以下，就拼接 age=18; 否则，就拼接年龄>18
+//    List<Account> queryAccountByCondition3(@Param("age") Integer age);
+}
