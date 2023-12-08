3715cc9dc23c4446a79e77e3ba5d1f351c08155f
stephen <sctuerprhyen@qq.com>
2023-12-08 20:17:20 +0800

    更新了mybatis作业

--- team/ZhouMuYang/src/InClass/Solution.java
+++ team/ZhouMuYang/src/InClass/Solution.java
@@ -1,52 +1,36 @@
 package InClass;
-
 import org.junit.Test;
+import java.util.Map.Entry;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 /**
  * @author stephen
  * @date 2023/11/24 9:29
  */
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-/**
- * @author stephen
- * @date 2023/11/23 23:45
- */
+//罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
 public class Solution {
-    public int maxProfit(int[] prices) {
-        boolean flag=true;
-        for (int i = 0; i < prices.length-1; i++) {
-            if (prices[i] < prices[i + 1]) {
-                flag=false;
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = s.length()-1; i >0 ; i--) {
+            Integer count = map.getOrDefault(s.charAt(i), 0);
+            map.put(s.charAt(i), count + 1);
+        }
+        System.out.println(map);
+        int sum=0;
+        Set<Entry<Character, Integer>> entrySet = map.entrySet();
+        for (Entry<Character, Integer> entry : entrySet) {
+            if (entry.getKey().equals("V")) {
+                sum+=entry.getValue();
             }
         }
-        if (flag) {
-            return 0;
-        }
-
-        int max = maxArray(prices);
-        int min = minArray(prices);
-        return max-min;
+        return sum;
     }
 
-    public int maxArray(int[] arr) {
-        int max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-    public int minArray(int[] arr) {
-        int min = arr[0];
-        for (int i = 1; i < arr.length-1; i++) {
-            if (min >arr[i]) {
-                min = arr[i];
-            }
-        }
-        return min;
+    @Test
+    public void testDemo() {
+        String s = "IIIVVV";
+        System.out.println(romanToInt(s));
     }
 }
 
